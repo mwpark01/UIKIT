0918b81d67fc9af389fc5776ffb1a02880feb9a9
mwpark01 <hyminwoo0000@gmail.com>
2025-03-13 11:30:17 +0900

    Label을 먼저 생성하고 UIStackView 생성 가능

--- UIViewDemo/UIViewDemo/ViewController.swift
+++ UIViewDemo/UIViewDemo/ViewController.swift
@@ -8,55 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupStackView()
-    }
-    
-    func setupStackView() {
-        let stackView = UIStackView()
-        stackView.backgroundColor = .lightGray
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        stackView.distribution = .fillEqually
-        stackView.spacing = 20
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        // 레이블 생성
-        let label1 = UILabel()
-        label1.backgroundColor = .systemRed
-        label1.text = "Label 1"
-        
-        let label2 = UILabel()
-        label2.backgroundColor = .systemBlue
-        label2.text = "Label 2"
-        
-        let label3 = UILabel()
-        label3.backgroundColor = .systemGreen
-        label3.text = "Label 3"
-        
-        // 레이블을 stackView에 추가
-        stackView.addArrangedSubview(label1)
-        stackView.addArrangedSubview(label2)
-        stackView.addArrangedSubview(label3)
-        
-        view.addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            // safeAreaLayoutGuide를 기준으로 top, leading, trailing, height 제약조건 설정
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            stackView.heightAnchor.constraint(equalToConstant: 200)
-        ])
-    }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupStackView()
+  }
+
+  func setupStackView() {
+    // 레이블 생성
+    let label1 = UILabel()
+    label1.backgroundColor = .systemRed
+    label1.text = "Label 1"
+
+    let label2 = UILabel()
+    label2.backgroundColor = .systemBlue
+    label2.text = "Label 2"
+
+    let label3 = UILabel()
+    label3.backgroundColor = .systemGreen
+    label3.text = "Label 3"
+
+    // 수직 스택 뷰 생성(레이블 3개를 먼저 생성 후 스택뷰 생성)
+    let stackView = UIStackView(arrangedSubviews: [label1, label2, label3])
+    stackView.backgroundColor = .lightGray
+    stackView.axis = .vertical
+    stackView.alignment = .center
+    stackView.distribution = .fillEqually
+    stackView.spacing = 20
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+
+    view.addSubview(stackView)
+
+    NSLayoutConstraint.activate([
+      // safeAreaLayoutGuide를 기준으로 top, leading, trailing, height 제약조건 설정
+      stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+      stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+      stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+      stackView.heightAnchor.constraint(equalToConstant: 200)
+    ])
+  }
 }
 
-// preview도 사용 가능!!
 #Preview {
-    ViewController()
+  ViewController()
 }
-
