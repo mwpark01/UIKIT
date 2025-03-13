3764cbc5fc34ca0c2781656d088121c513cba0c6
mwpark01 <hyminwoo0000@gmail.com>
2025-03-13 09:10:27 +0900

    Delegate 실습을 위한 프로젝트 생성

---
+++ DelegateDemo/DelegateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateDemo
+//
+//  Created by mwpark on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
