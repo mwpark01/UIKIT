e126a23dcce55f63fb60dd2fc21639b667d43a96
mwpark01 <hyminwoo0000@gmail.com>
2025-03-13 15:06:31 +0900

    셀 안에 버튼, 토글 추가

--- UIViewDemo/UIViewDemo/FormTableViewController.swift
+++ UIViewDemo/UIViewDemo/FormTableViewController.swift
@@ -8,68 +8,79 @@
 import UIKit
 
 class FormTableViewController: UITableViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        self.tableView.separatorColor = .black
+  var flag = false
+  let toggle = UISwitch()
+  let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height:30))
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    toggle.addTarget(self, action: #selector(toggleChanged), for: .valueChanged)
+    button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+
+    var config = UIButton.Configuration.filled()
+    config.baseBackgroundColor = .systemBlue
+    config.title = "Button"
+    button.isEnabled = flag
+    button.configuration = config
+
+    tableView.headerView(forSection: 0)?.backgroundColor = .systemRed
+
+  }
+
+  // MARK: - Table view data source
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Section Header"
+  }
+
+  override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    return "Section Footer"
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    print("\(indexPath.row)")
+    // 테이블 뷰 셀을 재사용 하거나 새로 생성
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+
+    if indexPath.row == 0 {
+      toggle.isOn = flag
+      cell.accessoryView = toggle
+    } else {
+      cell.accessoryView = button
     }
-    
-    // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 4
-    }
-    
-    // 섹션 하나에 들어가는 row 갯수
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        switch section {
-        case 0:
-            return 1
-        case 3:
-            return 3
-        default:
-            return 2
-        }
-    }
-    
-    // cell을 만들어준다.
-    // 해당 코드를 주석해제해서 직접 생성해줘야한다.
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 이전 코드는 재사용 코드를 불러오는 코드였으므로(해당 코드가 없음)
-        // 생성자로 만들어준다.
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        
-        // 셀 설정(설정을 셀에 삽입)
-        var config = UIListContentConfiguration.subtitleCell()
-        config.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
-        cell.contentConfiguration = config
-//        cell.backgroundColor = .systemGray
-        
-        return cell
-    }
-    
-    // 각 섹션의 높이
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        switch indexPath.section {
-        case 0:
-            return 100
-        case 1:
-            return 200
-        default:
-            return 50
-        }
-        
-    }
-    
+
+    // Configure the cell...
+    var config = UIListContentConfiguration.subtitleCell()
+    config.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
+    cell.contentConfiguration = config
+
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 50
+  }
+
+  // MARK: - objc selectors
+  @objc func toggleChanged(_ sender: UISwitch) {
+    flag = sender.isOn
+    button.isEnabled = flag
+  }
+
+  @objc func buttonTapped(_ sender: UIButton) {
+    print("Button tapped")
+  }
+
 }
 
 #Preview {
-    FormTableViewController(style: .insetGrouped)
+  FormTableViewController(style: .insetGrouped)
 }
