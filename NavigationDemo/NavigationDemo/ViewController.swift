f2748a885eafedade625ae9b926be6d173712d8c
mwpark <hyminwoo0000@gmail.com>
2025-03-18 15:52:31 +0900

    NavigationBar 설정

--- NavigationDemo/NavigationDemo/ViewController.swift
+++ NavigationDemo/NavigationDemo/ViewController.swift
@@ -12,6 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "네비게이션 타이틀"
+        
+        // ios 15 이상에서는 UINavigationBarAppearance 사용
+        let appearance = UINavigationBarAppearance()
+        // 투명한 배경 설정
+        appearance.configureWithOpaqueBackground()
+        // 배경색 설정
+        appearance.backgroundColor = .systemBlue
+        // 타이틀 색상
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        // 네비게이션 바에 적용(평상시)
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        // 스와이프로 스크롤할 때
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        self.navigationController?.navigationBar.tintColor = .white // 아이템 색상
+        
     }
 
 
