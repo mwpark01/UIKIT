f209748c37f3ae3f227bc49173aa29f0b694b035
mwpark <hyminwoo0000@gmail.com>
2025-03-18 13:41:31 +0900

    더블 탭 제스처 인식 기능 추가

--- GestureDemo/GestureDemo/ViewController.swift
+++ GestureDemo/GestureDemo/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        setupGesture()
     }
-
-
+    
+    func setupUI() {
+        let rectangle = UIView()
+        rectangle.backgroundColor = .yellow
+        rectangle.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rectangle)
+        
+        NSLayoutConstraint.activate([
+            rectangle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            rectangle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 100),
+            rectangle.widthAnchor.constraint(equalToConstant: 175),
+            rectangle.heightAnchor.constraint(equalToConstant: 125)
+        ])
+    }
+    
+    func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        tapGesture.numberOfTapsRequired = 2 // 더블 탭
+        tapGesture.numberOfTouchesRequired = 1 // 한 손가락
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTapGesture() {
+        print("더블 탭 제스처 인식")
+    }
+    
 }
-
