0f66fb01fac6686b0cb40f8feeb1a4d93161d9fd
mwpark <hyminwoo0000@gmail.com>
2025-03-19 09:52:19 +0900

    네비게이션 바 스타일 변경 및 서브 뷰 추가

--- NavigationDemo/NavigationDemo/ViewController.swift
+++ NavigationDemo/NavigationDemo/ViewController.swift
@@ -8,29 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "네비게이션 타이틀"
         
-        // ios 15 이상에서는 UINavigationBarAppearance 사용
-        let appearance = UINavigationBarAppearance()
-        // 투명한 배경 설정
-        appearance.configureWithOpaqueBackground()
-        // 배경색 설정
-        appearance.backgroundColor = .systemBlue
-        // 타이틀 색상
-        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.barStyle = .default
+        self.navigationController?.navigationBar.tintColor = .white
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.barTintColor = .systemBlue
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         
-        // 네비게이션 바에 적용(평상시)
-        self.navigationController?.navigationBar.standardAppearance = appearance
-        // 스와이프로 스크롤할 때
-        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        self.navigationController?.navigationBar.tintColor = .white // 아이템 색상
-        
+        // 네비게이션 큰 타이틀 설정
+        // 요즘 스타일의 네비게이션 타이틀임
+        //        self.navigationController?.navigationBar.prefersLargeTitles = true
+        //        self.navigationController?.navigationBar.backgroundColor = .systemBlue
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let VC = UIViewController()
+        VC.view.backgroundColor = .systemYellow
+        VC.title = "서브 타이틀"
+        self.navigationController?.pushViewController(VC, animated: true)
+    }
 }
-
+#Preview{
+    UINavigationController(rootViewController: ViewController())
+}
